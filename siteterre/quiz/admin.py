f1f016ca7cfa8d07e9e911c3018a93318514a006
StefanC <cotargasanu.stef@gmail.com>
2019-04-24 20:32:25 -0400

    Added Player and Question models

--- siteterre/quiz/admin.py
+++ siteterre/quiz/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Player, Question
 
-# Register your models here.
+admin.site.register(Player)
+admin.site.register(Question)
--- siteterre/quiz/models.py
+++ siteterre/quiz/models.py
@@ -1,3 +1,36 @@
+import json
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50)
+    group = models.IntegerField(db_index=True)
+    score = models.IntegerField(default=0)
+    lives = models.IntegerField(default=3)
+
+    def __str__(self):
+        return f'{self.username} from {self.group}'
+
+    def increase_score(self, increment):
+        self.score += increment
+
+    def decrease_lives(self, decrement):
+        self.lives -= decrement
+
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    question = models.CharField(max_length=300)
+    choice_1 = models.CharField(max_length=300)
+    choice_2 = models.CharField(max_length=300)
+    choice_3 = models.CharField(max_length=300)
+    choice_4 = models.CharField(max_length=300)
+    answer = models.CharField(max_length=500, default='[]')
+
+    def __str__(self):
+        return self.question
+
+    def set_answer(self, answer_list):
+        self.answer = json.dumps(answer_list)
+
+    def get_answer(self):
+        return json.loads(self.answer)
