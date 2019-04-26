45af3489dbab5849d4ca24ce5aa9c53e1bd74666
StefanC <cotargasanu.stef@gmail.com>
2019-04-26 15:27:07 -0400

    Added player group model

--- siteterre/quiz/admin.py
+++ siteterre/quiz/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Player, Question
+from django.contrib.auth.models import User, Group
+from .models import PlayerGroup, Player, Question
 
+# Register custom models
+admin.site.register(PlayerGroup)
 admin.site.register(Player)
 admin.site.register(Question)
+
+# Unregister the default models so they don't show up in the admin page
+admin.site.unregister(User)
+admin.site.unregister(Group)
--- siteterre/quiz/models.py
+++ siteterre/quiz/models.py
@@ -1,15 +1,21 @@
 import json
 from django.db import models
 
+class PlayerGroup(models.Model):
+    player_group = models.IntegerField(primary_key=True, db_index=True)
+
+    def __str__(self):
+        return f'{self.player_group}'
+
 class Player(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=50)
-    group = models.IntegerField(db_index=True)
+    player_group = models.ForeignKey(PlayerGroup, on_delete=models.CASCADE) # Each player is associated with a group
     score = models.IntegerField(default=0)
     lives = models.IntegerField(default=3)
 
     def __str__(self):
-        return f'{self.username} from {self.group}'
+        return f'{self.username} from {self.player_group}'
 
     def increase_score(self, increment):
         self.score += increment
@@ -29,8 +35,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question
 
+    # Create a json string to make an answer list
     def set_answer(self, answer_list):
         self.answer = json.dumps(answer_list)
 
+    # Use json to transform the list in string form to a usable list
     def get_answer(self):
         return json.loads(self.answer)
+
+    # Check if the given answer is part of the answer list
+    def verify_answer(self, given_answer):
+        correct_answer = self.get_answer()
+        return given_answer in correct_answer
